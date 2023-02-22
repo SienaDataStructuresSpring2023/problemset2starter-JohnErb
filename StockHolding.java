8bbfbfe4e608f975e5c04b4e191d68db62651cf6
jc07erb <jc07erb@siena.edu>
2023-02-22 18:24:50 -0500

    Part one done

--- StockHolding.java
+++ StockHolding.java
@@ -1,16 +1,57 @@
 /**
  * Write a description of class Asset here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (John Erb)
+ * @version (2/21/2023)
  */
 public class StockHolding
 {
+    private String symbol;
+    private String name;
+    private double numShares;
+    private double price;
     // @Override
-    // public String toString()
-    // {
+    public StockHolding(String symbol, String name, double numShares, double price){
+        this.symbol = symbol;
+        this.name = name;
+        this.numShares = numShares;
+        this.price = price;
+    }
+    
+    public String getSymbol(){
+        return symbol;    
+    }
+    
+    public String getName(){
+        return name;    
+    }
+    
+    public double getNumShares(){
+        return numShares;    
+    }
+    
+    public double getPrice(){
+        return price;    
+    }
+    
+    public void buyShares(double newShares, double newPrice){
+        numShares += newShares;
+        price = newPrice;
+    }
+    
+    public double sellShares(double shares){
+        double profit = 0;
+        if(numShares>= shares){
+            profit = shares*price;
+            numShares = numShares - shares;
+        }
+        return profit;
+    }
+    
+    public String toString()
+    {
         // //DO NOT EDIT THIS METHOD.
-        // return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        // symbol, name, numShares, price, numShares * price); 
-    // }
+        return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
+        symbol, name, numShares, price, numShares * price); 
+     }
 }
