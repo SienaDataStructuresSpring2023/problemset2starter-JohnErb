388db233792fb4dd2813f25dbd4f6e9787649a60
jc07erb <jc07erb@siena.edu>
2023-02-22 18:44:36 -0500

    Part 2 constructor and accessor methods

--- Portfolio.java
+++ Portfolio.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * Write a description of class Portfolio here.
@@ -7,18 +8,34 @@
  */
 public class Portfolio
 {
+    private ArrayList<StockHolding>stocks;
+    private double investment;
+    private double payout;
+    public Portfolio(){
+        ArrayList<StockHolding>stocks  = new ArrayList<StockHolding>();
+        investment = 0;
+        payout = 0;
+    }
+    public double getInvestment(){
+        return investment;
+    }
+    public double getPayout(){
+        return payout;
+    }
+    
     // @Override
-    // public String toString()
-    // {
+    public String toString()
+    {
         // //DO NOT EDIT THIS METHOD.
-        // StringBuffer sb = new StringBuffer();
-        // sb.append(String.format("%6s%25s%10s%15s%15s%n", 
-                // "Symbol", "Name", "Shares", "@Price", "Total"));
-        // sb.append("-----------------------------------------------------------------------\n");
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("%6s%25s%10s%15s%15s%n", 
+                "Symbol", "Name", "Shares", "@Price", "Total"));
+        sb.append("-----------------------------------------------------------------------\n");
 
-        // for(StockHolding s : stocks){
-            // sb.append(s.toString());
-        // }
-        // return sb.toString();
-    // }
+        for(StockHolding s : stocks){
+            sb.append(s.toString());
+            
+        }
+        return sb.toString();
+    }
 }
